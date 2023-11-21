8a4380e016cf9347e315b1ba1a58be1348d284c1
Klaudine <109423321+klaudine@users.noreply.github.com>
2023-11-20 19:49:02 -0800

    Update root.jsx

--- routes/root.jsx
+++ routes/root.jsx
@@ -1,49 +1,77 @@
-import { Outlet, NavLink, Link, useLoaderData, Form, redirect, useNavigation } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, NavLink, Link, useLoaderData, Form, redirect, useNavigation, useSubmit, } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 
-export async function action() {
-  const contact = await createContact();
-  return redirect(`/contacts/${contact.id}/edit`);
-}
 
+export async function action() {
+    const contact = await createContact();
+    return redirect(`/contacts/${contact.id}/edit`);
+  }
+
+  export async function loader({ request }) {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
+    return { contacts, q };
+  }
 
 export default function Root() {
-  const { contacts } = useLoaderData();
-  const navigation = useNavigation();
+    const { contacts, q } = useLoaderData();
+    const navigation = useNavigation();
+    const submit = useSubmit();
+
+    const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      "q"
+    );
+
+    useEffect(() => {
+        document.getElementById("q").value = q;
+      }, [q]);
+
     return (
       <>
         <div id="sidebar">
           <h1>React Router Contacts</h1>
           <div>
-            <form id="search-form" role="search">
+            <Form id="search-form" role="search">
               <input
                 id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
+                className={searching ? "loading" : ""}
+                // aria-label="Search contacts"
+                // placeholder="Search"
+                // type="search"
+                // name="q"
+                // defaultValue={q}
+                onChange={(event) => {
+                    const isFirstSearch = q == null;
+                    submit(event.currentTarget.form, {
+                      replace: !isFirstSearch,
+                    });
+                }}
               />
               <div
                 id="search-spinner"
                 aria-hidden
-                hidden={true}
+                hidden={!searching}
               />
               <div
                 className="sr-only"
                 aria-live="polite"
               ></div>
-            </form>
-            <Form method="post">
-              <button type="submit">New</button>
             </Form>
+            <Form method="post">
+            <button type="submit">New</button>
+          </Form>
           </div>
           <nav>
-            {contacts.length ? (
+          {contacts.length ? (
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <NavLink
+                    <NavLink
                     to={`contacts/${contact.id}`}
                     className={({ isActive, isPending }) =>
                       isActive
@@ -64,7 +92,6 @@ export default function Root() {
                     {contact.favorite && <span>★</span>}
                   </Link>
                   </NavLink>
-                  
                 </li>
               ))}
             </ul>
@@ -73,27 +100,15 @@ export default function Root() {
               <i>No contacts</i>
             </p>
           )}
-            <ul>
-              <li>
-                <Link to={`/contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                <Link to={`/contacts/2`}>Your Friend</Link>
-              </li>
-            </ul>
           </nav>
         </div>
         <div id="detail"
         className={
           navigation.state === "loading" ? "loading" : ""
-        }>
-          <Outlet />
+        }
+        >
+        <Outlet />
         </div>
       </>
     );
-  }
-
-  export async function loader() {
-    const contacts = await getContacts();
-    return { contacts };
   }
